912429c3f83359537223acbb1f3ca60de91249ab
knaD123 <amad18@student.aau.dk>
2022-03-22 14:39:33 +0100

    Update schedule-run-tool.sh

--- scripts/schedule-run-tool.sh
+++ scripts/schedule-run-tool.sh
@@ -7,7 +7,7 @@ source ${PD}/venv/bin/activate
 for TOPO in $(ls topologies) ; do
     TOPONAME=${TOPO/.json/}
     for CONF in $(ls confs/${TOPONAME}/${CONFIG_FILE}) ; do
-        scripts/run-tool.sh confs/${TOPONAME}/${CONF}
+        scripts/run-tool.sh ${CONF}
 	done
 done
 
