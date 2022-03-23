a9f9ddcb5fdddd1e569f70f0cb4be1d56c021ea8
knaD123 <amad18@student.aau.dk>
2022-03-23 10:59:06 +0100

    scheduule

--- scripts/run-tool.sh
+++ scripts/run-tool.sh
@@ -3,13 +3,16 @@
 #SBATCH --mail-user=amad18@student.aau.dk
 #SBATCH --output=/nfs/home/student.aau.dk/amad18/slurm-output/gen-net-%j.out 
 #SBATCH --error=/nfs/home/student.aau.dk/amad18/slurm-output/gen-net-%j.err
-#SBATCH --partition=dhabi,rome
+#SBATCH --partition=naples,dhabi,rome
 #SBATCH --mem=16G
+#SBATCH --time=00:30:00
 
 let "m=1024*1024"
 ulimit -v $m
 
 PD=$(pwd)
+
+source ${PD}/venv/bin/activate
 
 # # In case you write to auxiliary files, you can work in a temporary folder in /scratch (which is node-local).
 # U=$(whoami)
--- scripts/schedule-run-tool.sh
+++ scripts/schedule-run-tool.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+#SBATCH --mail-type=FAIL # Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --mail-user=amad18@student.aau.dk
+#SBATCH --output=/nfs/home/student.aau.dk/amad18/slurm-output/gen-net-%j.out
+#SBATCH --error=/nfs/home/student.aau.dk/amad18/slurm-output/gen-net-%j.err
+#SBATCH --partition=naples,dhabi,rome
+#SBATCH --mem=16G
 
 PD=$(pwd)
 CONFIG_FILE="${1}.yml"
 
-source ${PD}/venv/bin/activate
-
 for TOPO in $(ls topologies) ; do
     TOPONAME=${TOPO/.json/}
     for CONF in $(ls confs/${TOPONAME}/${CONFIG_FILE}) ; do
-        scripts/run-tool.sh ${CONF}
+        sbatch scripts/run-tool.sh ${CONF}
 	done
 done
 
