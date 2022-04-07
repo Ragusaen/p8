9a2d38e2915b118e4b3879d3026df5e8f7de4c91
knaD123 <amad18@student.aau.dk>
2022-04-07 16:39:41 +0200

    fix tba

--- scripts/run-tool.sh
+++ scripts/run-tool.sh
@@ -2,7 +2,9 @@
 #SBATCH --mail-type=NONE # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=amad18@student.aau.dk
 ###SBATCH --output=/dev/null
-#SBATCH --partition=dhabi
+#SBATCH --output=/nfs/home/student.aau.dk/amad18/slurm-output/run-tool-%j.out
+#SBATCH --error=/nfs/home/student.aau.dk/amad18/slurm-output/run-tool-%j.err
+#SBATCH --partition=rome
 #SBATCH --mem=4G
 #SBATCH --cpus-per-task=1
 
@@ -15,7 +17,5 @@ python3 -m pip install -r requirements.txt
 CONFIG_FILE_YML="${1}.yml"
 CONFIG_FILE="${1}"
 FAILCHUNK="$2"
-
-python3 --version
 
 python3 ${PD}/tool_simulate.py --conf confs/${CONFIG_FILE_YML} --failure_chunk_file ${FAILCHUNK} --result_folder "results/${CONFIG_FILE}"
