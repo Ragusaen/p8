6c48636ce7982de7bc726e6f11e748328e6bc75f
knaD123 <amad18@student.aau.dk>
2022-04-11 11:11:18 +0200

    Update run-problems-subset.sh

--- scripts/run-problems-subset.sh
+++ scripts/run-problems-subset.sh
@@ -3,15 +3,18 @@
 PD=$(pwd)
 CONFIG_FILE="${1}"
 AMOUNT_TO_TEST="${2}"
-count=0
+count=1
 
 source ~/p8/venv/bin/activate
 
 for TOPO in $(ls confs) ; do
-    if [ AMOUNT_TO_TEST > count ]
+    if [ $count -le $AMOUNT_TO_TEST ]
     then
         for FAILCHUNK in $(ls confs/${TOPO}/failure_chunks) ; do
-            python3 ${PD}/tool_simulate.py --conf "confs/${TOPO}/${CONFIG_FILE}.yml" --failure_chunk_file "confs/${TOPO}/failure_chunks/${FAILCHUNK}" --result_folder "results/${CONFIG_FILE}"
+            python3 ${PD}/tool_simulate.py --conf "confs/${TOPO}/${CONFIG_FILE}.yml" --failure_chunk_file "confs/${TOPO}/failure_chunks/${FAILCHUNK}" --result_folder "results/${TOPO}/${CONFIG_FILE}"
         done
     fi
+    (( count++ ))
 done
+
+rm count
