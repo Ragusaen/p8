d029e6503c283481408d7a2e3e885e35ee84b7f8
Slorup <nicklas.slorup@yahoo.dk>
2022-04-13 12:44:24 +0200

    Optimise results parsing a lil bit

--- get_results.py
+++ get_results.py
@@ -32,17 +32,45 @@ class TopologyResult:
         self.failure_chunks = failure_chunks
         self.connectedness = connectedness
 
-def compute_probability(f, e, pf=0.5):
+def __compute_probability(f, e, pf=0.5):
     return (pf ** f) * (1 - pf) ** (e - f)
+
+def __parse_single_line_in_failure_scenario(line):
+    parts = line.split(' ')
+    for part in parts:
+        split = part.split(':')
+        prop_name = split[0]
+        value = split[1]
+
+        if (prop_name == 'len(F)'): #No match/case in this version :(
+            failed_links = int(value)
+            continue
+        if (prop_name == 'len(E)'):
+            total_links = int(value)
+            continue
+        if (prop_name == 'ratio'):
+            connectivity = float(value)
+            continue
+        if (prop_name == 'looping_links'):
+            looping_links = int(value)
+            continue
+        if (prop_name == 'num_flows'):
+            num_flows = int(value)
+            continue
+        if (prop_name == 'successful_flows'):
+            successful_flows = int(value)
+            continue
+        if (prop_name == 'connected_flows'):
+            connected_flows = int(value)
+            continue
+    return FailureScenarioData(failed_links, total_links, connectivity, looping_links, num_flows,
+                                       successful_flows, connected_flows)
+
 
 def parse_result_data(result_folder):
     result_dict = {}
     conf_progress = 1
-    hello = 0
     for conf_name in os.listdir(result_folder):
-        if hello > 6:
-            continue
-        hello += 1
         print(f"\nParsing results from algorithm {alg_full_name_dict[conf_name]} - {conf_progress}/{len(os.listdir(result_folder))}")
         conf_progress += 1
         result_dict[conf_name] = {}
@@ -54,20 +82,11 @@ def parse_result_data(result_folder):
             for res_file in os.listdir(res_dir):
                 failure_scenarios = list()
                 with open(f"{res_dir}/{res_file}", "r") as t:
-                    l = t.readline()
-                    while l:
-                        failed_links = int(re.findall(r'len\(F\):(\d+)', l)[0])
-                        total_links = int(re.findall(r'len\(E\):(\d+)', l)[0])
-                        connectivity = float(re.findall(r'ratio:(\d+\.\d+)', l)[0])
-                        looping_links = int(re.findall(r'looping_links:(\d+)', l)[0])
-                        num_flows = int(re.findall(r'num_flows:(\d+)', l)[0])
-                        successful_flows = int(re.findall(r'successful_flows:(\d+)', l)[0])
-                        connected_flows = int(re.findall(r'connected_flows:(\d+)', l)[0])
-                        failure_data = FailureScenarioData(failed_links, total_links, connectivity, looping_links,
-                                                 num_flows,
-                                                 successful_flows, connected_flows)
+                    lines = t.readlines()
+                    for line in lines:
+                        failure_data = __parse_single_line_in_failure_scenario(line)
 
-                        p = compute_probability(failure_data.failed_links, failure_data.total_links)
+                        p = __compute_probability(failure_data.failed_links, failure_data.total_links)
                         normalisation_sum += p
 
                         if failure_data.connected_flows != 0:
@@ -76,7 +95,6 @@ def parse_result_data(result_folder):
                             connectedness += p
 
                         failure_scenarios.append(failure_data)
-                        l = t.readline()
 
                 failure_chunks.append(FailureChunkResultData(res_file, failure_scenarios))
 
